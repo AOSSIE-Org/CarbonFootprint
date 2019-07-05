e06f649efff72fad090e39631585d74c92e1499b
aayushdutt <aayushd100@gmail.com>
2019-07-06 00:43:41 +0530

    add tests for googleflights, expedia, tripadvisor, united flight booking websites

--- Spec/tests/flights.test.js
+++ Spec/tests/flights.test.js
@@ -21,104 +21,170 @@ afterAll(() => {
   browser.close();
 });
 
+// Get current Month and Year
 var currDate = new Date()
 var nextMonth = currDate.getMonth() + 2;
 var year = currDate.getFullYear();
+var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
 if(nextMonth === 13) {
     nextMonth = 1;
     year += 1;
 }
-
+var nextMonthName = monthNames[nextMonth - 1]
 if(nextMonth <= 10) {
     // pad Month with 0. (8 -> 08)
     nextMonth = '0' + nextMonth
 }
 
-// test("Cleartrip Fligts", async () => {
-//   const data = flightsData.cleartrip;
-//   let page = await browser.newPage();
-//   await page.goto(data.url + `01/${nextMonth}/${year}`, {waitUntil: 'load', timeout: 0});
 
-//   await page.waitFor('#carbon');
-//   const emission = await page.$eval("#carbon", el => el.innerText)
-//   const emissionFloat = parseFloat(emission)
-//   console.log("Bing Maps Emission: ", emissionFloat) 
-//   expect(emissionFloat).toBeGreaterThan(0);
-//   page.close();
-// }, 50000);
+// --------------TESTS---------------------
+test("Sky Scanner", async () => {
+    // Extension not working
+    const data = flightsData.skyscanner;
+    let page = await browser.newPage();
+    await page.goto(data.url.split('|').join(`${year%100}${nextMonth}01`) , {waitUntil: 'load', timeout: 0});
+  
+    await page.waitFor('#carbon');
+    const emission = await page.$eval("#carbon", el => el.innerText)
+    const emissionFloat = parseFloat(emission)
+    console.log("Sky Scanner Emission: ", emissionFloat) 
+    expect(emissionFloat).toBeGreaterThan(0);
+    page.close();
+}, 50000);
 
+test("Cleartrip Fligts", async () => { //working and tests passing
+  const data = flightsData.cleartrip;
+  let page = await browser.newPage();
+  await page.goto(data.url.split('|').join(`01/${nextMonth}/${year}`) , {waitUntil: 'load', timeout: 0});
+
+  await page.waitFor('#carbon');
+  const emission = await page.$eval("#carbon", el => el.innerText)
+  const emissionFloat = parseFloat(emission)
+  console.log("Cleartrip Fligts Emission: ", emissionFloat) 
+  expect(emissionFloat).toBeGreaterThan(0);
+  page.close();
+}, 50000);
+
+test("Hipmunk Flights", async () => {
+    //Extension is Not working
+    const data = mapsData.hipmunk;
+    let page = await browser.newPage();
+    await page.goto(data.url.split('|').join(`${year}-${nextMonth}-01`), {waitUntil: 'load', timeout: 0});
+  
+    await page.waitFor('#carbon');
+    const emission = await page.$eval("#carbon", el => el.innerText)
+    const emissionFloat = parseFloat(emission)
+    console.log("Hipmunk Emission: ", emissionFloat) 
+    expect(emissionFloat).toBeGreaterThan(0);
+    page.close();
+  }, 50000);
+
+test("Makemytrip Fligts", async () => {
+    //extension not working
+  const data = flightsData.makemytrip;
+  let page = await browser.newPage();
+  await page.goto(data.url.split('|').join(`01/${nextMonth}/${year}`), {waitUntil: 'load', timeout: 0});
+
+  await page.waitFor('#carbon');
+  const emission = await page.$eval("#carbon", el => el.innerText)
+  const emissionFloat = parseFloat(emission)
+  console.log("Makemytrip Emission: ", emissionFloat) 
+  expect(emissionFloat).toBeGreaterThan(0);
+  page.close();
+}, 50000);
 
 test("Google Flights", async () => {
     //extension not working on some websites
   const data = flightsData.googleflights;
   let page = await browser.newPage();
-  await page.goto(data.url + `${year}-${nextMonth}-01;c:INR;e:1;sd:1;t:f;tt:o` , {waitUntil: 'load', timeout: 0});
+  await page.goto(data.url.split('|').join(`${year}-${nextMonth}-01;`) , {waitUntil: 'load', timeout: 0});
 
   await page.waitFor('#carbon');
   const emission = await page.$eval("#carbon", el => el.innerText)
   const emissionFloat = parseFloat(emission)
-  console.log("Bing Maps Emission: ", emissionFloat) 
+  console.log("Google Flights Emission: ", emissionFloat) 
   expect(emissionFloat).toBeGreaterThan(0);
   page.close();
 }, 50000);
 
+test("Expedia Flights", async () => { // working and tests passing
+  const data = flightsData.expedia;
+  let page = await browser.newPage();
+  await page.goto(data.url.split('|').join(`${nextMonth}%2F01%2F${year}`) , {waitUntil: 'load', timeout: 0});
 
-// test("Makemytrip Fligts", async () => {
-//     //extension not working
-//   const data = flightsData.makemytrip;
-//   let page = await browser.newPage();
-//   await page.goto(data.url + `01/${nextMonth}/${year}`, {waitUntil: 'load', timeout: 0});
+  await page.waitFor('#carbon');
+  const emission = await page.$eval("#carbon", el => el.innerText)
+  const emissionFloat = parseFloat(emission)
+  console.log("Expedia Flights Emission: ", emissionFloat) 
+  expect(emissionFloat).toBeGreaterThan(0);
+  page.close();
+}, 50000);
 
-//   await page.waitFor('#carbon');
-//   const emission = await page.$eval("#carbon", el => el.innerText)
-//   const emissionFloat = parseFloat(emission)
-//   console.log("Bing Maps Emission: ", emissionFloat) 
-//   expect(emissionFloat).toBeGreaterThan(0);
-//   page.close();
-// }, 50000);
-
-
-// test("Hipmunk Flights", async () => {
-//   //Extension is Not working
-//   const data = mapsData.hipmunk;
-//   let page = await browser.newPage();
-//   await page.goto(data.url + `${year}-${nextMonth}-01`, {waitUntil: 'load', timeout: 0});
-
-//   await page.waitFor('#carbon');
-//   const emission = await page.$eval("#carbon", el => el.innerText)
-//   const emissionFloat = parseFloat(emission)
-//   console.log("Bing Maps Emission: ", emissionFloat) 
-//   expect(emissionFloat).toBeGreaterThan(0);
-//   page.close();
-// }, 50000);
-
-
-
-
-// test("Sky Scanner", async () => {
-    //Website does not allow bots
-//   const data = mapsData.skyscanner;
-//   let page = await browser.newPage();
-//   await page.goto(data.url, {waitUntil: 'load', timeout: 0});
-
-//   //type source
-//   await page.waitForSelector('input#fsc-origin-search')
-//   await page.click('input#fsc-origin-search')
-//   await page.keyboard.type('New Delhi');
-//   await page.keyboard.press('Enter');
-//   //type destination
-//   await page.waitForSelector('input#fsc-destination-search')
-//   await page.click('input#fsc-destination-search')
-//   await page.keyboard.type('New York');
-//   await page.keyboard.press('Enter');
-//   //submit
-//   await page.click('button[type="submit"]')
+test("Tripadvisor Flights", async () => {
+    // Extension not working
+    const data = flightsData.tripadvisor;
+    let page = await browser.newPage();
+    await page.goto(data.url.split('|').join(`${year}${nextMonth}01`) , {waitUntil: 'load', timeout: 0});
   
-//   await page.waitFor('#carbon');
-//   const emission = await page.$eval("#carbon", el => el.innerText)
-//   const emissionFloat = parseFloat(emission)
-//   console.log("Bing Maps Emission: ", emissionFloat) 
-//   expect(emissionFloat).toBeGreaterThan(0);
-//   page.close();
-// }, 50000);
+    await page.waitFor('#carbon');
+    const emission = await page.$eval("#carbon", el => el.innerText)
+    const emissionFloat = parseFloat(emission)
+    console.log("Tripadvisor Flights Emission: ", emissionFloat) 
+    expect(emissionFloat).toBeGreaterThan(0);
+    page.close();
+}, 50000);
+
+test("United Flights", async () => { //working and tests passing
+    const data = flightsData.united;
+    let page = await browser.newPage();
+    await page.setViewport({
+        width: 1000,
+        height: 800,
+        deviceScaleFactor: 1,
+    });
+    await page.goto(data.url , {waitUntil: 'domcontentloaded', timeout: 0});
+    
+    // ---simulate human interaction---
+    var onewaySelector = 'label[for="oneway"]'
+    var originSelector = 'input#bookFlightOriginInput'
+    var originLabelSelector = 'button[aria-label="Delhi, IN (DEL)"]'
+    var destinationSelector = 'input#bookFlightDestinationInput'
+    var destinationLabelSelector = 'button[aria-label="New York, NY, US (NYC - All Airports)"]'
+    var dateSelector = 'input#DepartDate'
+    var submitButtonSelector = 'form#bookFlightForm button[type="submit"]'
+    
+    await page.waitForSelector(onewaySelector)
+    await page.click(onewaySelector)
+    
+    await page.waitForSelector(originSelector)
+    await page.click(originSelector)
+    await page.keyboard.type('Delhi');
+    await page.waitForSelector(originLabelSelector)
+    await page.click(originLabelSelector)
+    
+    await page.click(destinationSelector)
+    await page.keyboard.type('New Yor');
+    await page.waitForSelector(destinationLabelSelector)
+    await page.click(destinationLabelSelector)
+    
+    await page.click(dateSelector)
+    const dateValue = await page.$eval(dateSelector, el => el.value);
+    for (let i = 0; i < dateValue.length; i++) {
+        await page.keyboard.press('Backspace');
+    }
+    await page.keyboard.type(nextMonthName + ' 01');
+    await page.keyboard.press('Enter');
+    await page.click(submitButtonSelector)
+    
+    // ----perform test----
+    await page.waitFor('#carbon');
+    const emission = await page.$eval("#carbon", el => el.innerText)
+    const emissionFloat = parseFloat(emission)
+    console.log("United Flights Emission: ", emissionFloat) 
+    expect(emissionFloat).toBeGreaterThan(0);
+    page.close();
+}, 100000);
   
