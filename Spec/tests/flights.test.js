1deacf8c7fedef6e11ae7399d45cfbf85a3a2061
aayushdutt <aayushd100@gmail.com>
2019-07-06 14:30:46 +0530

    add tests for kayak, lufthansa, amadues, delta and spicejet

--- Spec/tests/flights.test.js
+++ Spec/tests/flights.test.js
@@ -12,7 +12,7 @@ beforeAll(async () => {
       `--load-extension=${CRX_PATH}`
     ]
   });
-
+  
   pages = await browser.pages();
   pages[0].close();
 });
@@ -113,7 +113,7 @@ test("Google Flights", async () => {
 test("Expedia Flights", async () => { // working and tests passing
   const data = flightsData.expedia;
   let page = await browser.newPage();
-  await page.goto(data.url.split('|').join(`${nextMonth}%2F01%2F${year}`) , {waitUntil: 'load', timeout: 0});
+  await page.goto(data.url.split('|').join(`${nextMonth}%2F01%2F${year}`) , {waitUntil: 'domcontentloaded', timeout: 0});
 
   await page.waitFor('#carbon');
   const emission = await page.$eval("#carbon", el => el.innerText)
@@ -127,7 +127,7 @@ test("Tripadvisor Flights", async () => {
     // Extension not working
     const data = flightsData.tripadvisor;
     let page = await browser.newPage();
-    await page.goto(data.url.split('|').join(`${year}${nextMonth}01`) , {waitUntil: 'load', timeout: 0});
+    await page.goto(data.url.split('|').join(`${year}${nextMonth}01`) , {waitUntil: 'domcontentloaded', timeout: 0});
   
     await page.waitFor('#carbon');
     const emission = await page.$eval("#carbon", el => el.innerText)
@@ -187,4 +187,225 @@ test("United Flights", async () => { //working and tests passing
     expect(emissionFloat).toBeGreaterThan(0);
     page.close();
 }, 100000);
+
+test("Spice Jet", async () => { 
+    // extension is not working
+    const data = flightsData.spicejet;
+    let page = await browser.newPage();
+    await page.setViewport({
+        width: 1000,
+        height: 800,
+        deviceScaleFactor: 1,
+    });
+    await page.goto(data.url , {waitUntil: 'domcontentloaded', timeout: 0});
+    
+    // ---simulate human interaction---
+    var originLabelSelector = 'input#ControlGroupSearchView_AvailabilitySearchInputSearchVieworiginStation1_CTXT'
+    var originSelector = 'a[value="DEL"]'  // Delhi
+    
+    var destinationLabelSelector = 'input#ControlGroupSearchView_AvailabilitySearchInputSearchViewdestinationStation1_CTXT'
+    var destinationSelector = '.destination div#glsControlGroupSearchView_AvailabilitySearchInputSearchViewdestinationStation1_CTNR a[value="BOM"]' // Bombay
+    
+    var dateLabelSelector = 'input.custom_date_pic'
+    var dateSelector = 'div.ui-datepicker-group.ui-datepicker-group-last tbody tr td a' 
+    
+    var submitButtonSelector = 'input.bookbtn'
+    
+    await page.waitForSelector(originLabelSelector)
+    await page.click(originLabelSelector)
+    await page.click(originSelector)
+
+    await page.click(destinationLabelSelector)
+    await page.click(destinationSelector)
+
+    await page.click(dateLabelSelector)
+    await page.click(dateSelector)
+
+    await page.click(submitButtonSelector)
+    
+    // ----perform test----
+    await page.waitFor('#carbon');
+    const emission = await page.$eval("#carbon", el => el.innerText)
+    const emissionFloat = parseFloat(emission)
+    console.log("Spice Jet Emission: ", emissionFloat) 
+    expect(emissionFloat).toBeGreaterThan(0);
+    page.close();
+}, 100000);
   
+
+test("Kayak Flights", async () => {
+  const data = flightsData.kayak;
+  let page = await browser.newPage();
+  await page.goto(data.url.split('|').join(`${year}-${nextMonth}-01`) , {waitUntil: 'domcontentloaded', timeout: 0});
+
+  await page.waitFor('#carbon');
+  const emission = await page.$eval("#carbon", el => el.innerText)
+  const emissionFloat = parseFloat(emission)
+  console.log("Kayak Flights Emission: ", emissionFloat) 
+  expect(emissionFloat).toBeGreaterThan(0);
+  page.close();
+}, 50000);
+
+
+test("Amadues", async () => { // tests passing
+  const data = flightsData.amadues;
+  let page = await browser.newPage();
+  await page.goto(data.url, {waitUntil: 'load', timeout: 0});
+
+  // -------SELECTORS-------------
+  var onewaySelector = 'label[for="flightSearchForm.tripType.oneWay"]';
+  var originLabelSelector = 'input[id="reservationFlightSearchForm.originAirport"]';
+  var destinationLabelSelector = 'input[id="reservationFlightSearchForm.destinationAirport"]';
+  var dateLabelSelector = 'input[id="aa-leavingOn"]';
+  var submitButtonSelector = 'input[type="submit"]'
+  var continueButtonSelector = "a#CLARbtnSearch";
+
+  // ----------HUMAN INTERACTION---------------
+  await page.waitForSelector(onewaySelector)
+  await page.click(onewaySelector)
+  
+  await page.click(originLabelSelector)
+  await page.keyboard.type('New York');
+  
+  await page.click(destinationLabelSelector)
+  await page.keyboard.type('San Fransisco');
+  
+  await page.click(dateLabelSelector)
+  const dateValue = await page.$eval(dateLabelSelector, el => el.value);
+  for (let i = 0; i < dateValue.length; i++) {
+    await page.keyboard.press('Backspace');
+  }
+  await page.keyboard.type(`01/${nextMonth}/${year}`);
+  await page.keyboard.press('Enter');
+  await page.click(submitButtonSelector)
+  
+  await page.waitForSelector(continueButtonSelector)
+  await page.click(continueButtonSelector)
+
+  // -----------PERFORM TESTS------------------------
+  await page.waitFor('#carbon');
+  const emission = await page.$eval("#carbon", el => el.innerText)
+  const emissionFloat = parseFloat(emission)
+  console.log("Amadues Emission: ", emissionFloat) 
+  expect(emissionFloat).toBeGreaterThan(0);
+  page.close();
+}, 50000);
+
+
+
+test("Delta", async () => { 
+  // extension is not working
+  const data = flightsData.delta;
+  let page = await browser.newPage();
+  await page.setViewport({
+      width: 1000,
+      height: 800,
+      deviceScaleFactor: 1,
+  });
+  await page.goto(data.url , {waitUntil: 'load', timeout: 0});
+  
+  // ---simulate human interaction---
+  var originLabelSelector = 'a#fromAirportName'
+  var originSelector = 'li.airport-list'
+
+  var destinationLabelSelector = 'a#toAirportName'
+  var destinationSelector = 'li.airport-list'
+  
+  var onewayLabelSelector = 'span[aria-owns="selectTripType-desc"]'
+  var onewaySelector = 'li[data="1"]'
+  
+  var dateLabelSelector = 'input.calendarInput'
+  var dateSelector = '.dl-datepicker-group.dl-datepicker-group-1 tbody tr + tr td a'
+  
+  var submitButtonSelector = 'button[type="submit"]'
+  
+  await page.waitForSelector(originLabelSelector)
+  await page.click(originLabelSelector)
+  await page.keyboard.type('DEL');
+  await page.waitForSelector(originSelector)
+  await page.click(originSelector)
+  
+  await page.click(destinationLabelSelector)
+  await page.keyboard.type('NYC');
+  await page.waitForSelector(destinationSelector)
+  await page.click(destinationSelector)
+  
+  await page.click(onewayLabelSelector)
+  await page.waitForSelector(onewaySelector)
+  await page.click(onewaySelector)
+  
+  await page.click(dateLabelSelector)
+  await page.waitForSelector(dateSelector)
+  await page.click(dateSelector)
+
+  await page.click(submitButtonSelector)
+  
+  // ----perform test----
+  await page.waitFor('#carbon');
+  const emission = await page.$eval("#carbon", el => el.innerText)
+  const emissionFloat = parseFloat(emission)
+  console.log("Delta Emission: ", emissionFloat) 
+  expect(emissionFloat).toBeGreaterThan(0);
+  page.close();
+}, 100000);
+
+
+// test("Lufthansa Flights", async () => { 
+//   //Extension not working
+//   // Doesn't allow bots!
+//   const data = flightsData.lufthansa;
+//   let page = await browser.newPage();
+//   await page.setViewport({
+//       width: 1000,
+//       height: 800,
+//       deviceScaleFactor: 1,
+//   });
+//   await page.goto(data.url , {waitUntil: 'domcontentloaded', timeout: 0});
+  
+//   // ---simulate human interaction---
+//   var confirmCookieSelector = 'button.cm-selectSpecific'
+
+//   var originLabelSelector = 'input[placeholder="From"]'
+//   var originSelector = 'div.airport-text-code'
+  
+//   var destinationLabelSelector = 'input[placeholder="To"]'
+//   var destinationSelector = 'div.airport-text-code'
+//   // Chhatrapati Shivaji Maharaj International
+
+//   var onewaySelector = 'input[name="isOneWay"]'
+//   var dateLabelSelector = 'input[placeholder="Departure date"]'
+//   var dateSelector = 'div + div.CalendarMonthGrid_month__horizontal.CalendarMonthGrid_month__horizontal_1 + div tr td[role="button"]'
+//   var submitButtonSelector = 'button[type="submit"]'
+  
+//   console.log("waiting for button")
+//   await page.waitForSelector(confirmCookieSelector)
+//   await page.click(confirmCookieSelector)
+//   console.log("confirmed cookeis")
+
+  
+//   await page.click(originLabelSelector)
+//   await page.keyboard.type('New Delhi');
+//   await page.waitForSelector(originSelector)
+//   await page.click(originSelector)
+  
+//   await page.click(destinationLabelSelector)
+//   await page.keyboard.type('Chhatrapati Shivaji Maharaj International');
+//   await page.waitForSelector(destinationSelector)
+//   await page.click(destinationSelector)
+  
+//   await page.click(onewaySelector)
+  
+//   await page.click(dateLabelSelector)
+//   await page.waitForSelector(dateSelector)
+//   await page.click(dateSelector)
+
+//   await page.click(submitButtonSelector)
+  
+//   // ----perform test----
+//   await page.waitFor('#carbon');
+//   const emission = await page.$eval("#carbon", el => el.innerText)
+//   const emissionFloat = parseFloat(emission)
+//   console.log("Lufthansa Flights Emission: ", emissionFloat) 
+//   expect(emissionFloat).toBeGreaterThan(0);
+//   page.close();
+// }, 100000);
