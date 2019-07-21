a4f1e3a9b4529b58a0a51091b1642ab061ee69b9
aayushdutt <aayushd100@gmail.com>
2019-07-21 23:28:49 +0530

    move trentialia

--- Spec/tests-non-ci/trains.test.js
+++ Spec/tests-non-ci/trains.test.js
@@ -40,3 +40,55 @@ if(nextMonth <= 10) {
     nextMonth = '0' + nextMonth
 }
 
+
+test("Trenitalia", async () => { 
+  // message port was closed before a message was recieved
+  const data = trainsData.trenitalia;
+  let page = await browser.newPage();
+  await blockImages(page)
+  await page.setViewport({
+    width: 1200,
+    height: 700,
+    deviceScaleFactor: 1,
+  });
+  await page.goto(data.url, {waitUntil: 'load', timeout: 0});
+
+  // -----------SELECTORS-------------
+  
+  var departureLabel = 'input[name="departureStation"]'
+  var departure = 'ul.ui-autocomplete li'
+  
+  var arrivalLabel = 'input[name="arrivalStation"]'
+  var arrival = 'ul.ui-autocomplete#ui-id-2 li'
+  
+  var dateLabel = 'input[name="departureDate"]'
+  var submitButton = 'button[title="Cerca"]'
+
+  // ---- HUMAN INTERACTION --------
+  await page.waitForSelector(departureLabel, {timeout: 70000})
+  await page.click(departureLabel)
+  await page.keyboard.type('Vene');
+  await page.waitForSelector(departure)
+  await page.click(departure)
+  
+  await page.click(arrivalLabel)
+  await page.keyboard.type('Roma');
+  await page.waitForSelector(arrival)
+  await page.click(arrival)
+
+  await page.click(dateLabel)
+  const dateValue = await page.$eval(dateLabel, el => el.value);
+  for (let i = 0; i < dateValue.length; i++) {
+      await page.keyboard.press('Backspace');
+  }
+  await page.keyboard.type(`01-${nextMonth}-${year}`);
+  await page.click(submitButton)
+
+  // ---------- PERFORM TESTS-------------
+  await page.waitFor('#carbon', {timeout: 100000});
+  const emission = await page.$eval("#carbon", el => el.innerText)
+  const emissionFloat = parseFloat(emission)
+  console.log("Trenitalia Emission: ", emissionFloat) 
+  expect(emissionFloat).toBeGreaterThan(0);
+  page.close();
+}, 120000);
